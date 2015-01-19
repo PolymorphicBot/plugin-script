92c244d5fc3c3322f23861a7250de5bd6df32f37
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-18 21:14:37 -0500

    First Test of dart2js support.

--- script.dart
+++ script.dart
@@ -4,6 +4,7 @@ import "dart:convert";
 import "dart:io";
 
 import "package:polymorphic_bot/api.dart";
+import "package:compiler_unsupported/src/dart2js.dart";
 
 BotConnector bot;
 
@@ -60,6 +61,76 @@ void main(List<String> args, Plugin plugin) {
       });
     });
   }, permission: "eval");
+  
+  bot.command("eval-dart2js", (event) {
+    File file = new File("/tmp/script${new Random().nextInt(4000)}.dart");
+
+    if (file.existsSync()) {
+      file.deleteSync();
+    }
+
+    List<String> imports = [
+      "dart:async",
+      "dart:io",
+      "dart:convert",
+      "dart:math",
+      "dart:mirrors",
+      "dart:typed_data",
+      "https://gist.githubusercontent.com/kaendfinger/03a43678776d9a906e88/raw/functions.dart"
+    ];
+    
+    var mydir = new File.fromUri(Platform.script).parent;
+    var preamble = new File("${mydir.path}/preamble.js");
+    
+    var input = event.args.join(" ");
+    String code;
+    
+    if (input.length > 1 && input[0] == "@") {
+      input = input.substring(1);
+      code = input;
+    } else {
+      code = imports.map((it) => "import '${it}';").join("\n") + "\nvoid main() {" + event.args.join(" ") + "}";
+    }
+    
+    var tmpDir = Directory.systemTemp.createTempSync("eval-dart2js");
+    var out = new File("${tmpDir.path}/out.js");
+    
+    file.writeAsStringSync(code);
+    
+    compilerMain(["--categories=Server", "-o", out.path, file.path]).then((result) {
+      if (!result.isSuccess) {
+        event.reply("> Compilation Failed!");
+        file.deleteSync();
+        tmpDir.deleteSync(recursive: true);
+        return null;
+      }
+      
+      out.writeAsStringSync(preamble.readAsStringSync() + "\n" + out.readAsStringSync());
+      
+      return Process.start("node", [out.path]);
+    }).then((proc) {
+      if (proc == null) {
+        return;
+      }
+      
+      proc.stdout.transform(UTF8.decoder).transform(new LineSplitter()).listen((data) {
+        event.reply("> ${data}");
+      });
+
+      proc.stderr.transform(UTF8.decoder).transform(new LineSplitter()).listen((data) {
+        event.replyNotice("> ${data}");
+      });
+
+      new Timer(new Duration(seconds: 5), () {
+        if (proc.kill()) {
+          event.reply("> Script was killed (over 5 seconds of execution time)");
+        }
+
+        file.deleteSync();
+        tmpDir.deleteSync(recursive: true);
+      });
+    });;
+  }, permission: "eval-dart2js");
 
   bot.command("js-eval", (event) {
     File file = new File("/tmp/script${new Random().nextInt(4000)}.js");
